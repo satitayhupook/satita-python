5693f0965f39e9c6716467a1099dec5dea62c7ab
Satita Yhupook <satita.y@ku.th>
2025-07-17 15:12:11 +0700

    stt

--- assignments/week01/lab01.py
+++ assignments/week01/lab01.py
@@ -7,17 +7,3 @@ print("=== Personal Profile Creator ===")
 # - Phone number
 # - Favorite hobby
 # Then display it as a profile
-
-# Write your solution here:
-full_name = input("What is your name? :")
-age = input("How old are you? :")
-email = input("What is your email address? :")
-phone_number = input("What is your phone number? :")
-hobby = input("What is your faverite hobby? :")
-
-print("=== Your pofile ===")
-print("Full name: ", full_name)
-print("Age: ", age)
-print("Email ", email)
-print("Phone Number: ", phone_number)
-print("Faverite Hobby: ", hobby)
