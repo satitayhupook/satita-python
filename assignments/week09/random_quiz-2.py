f9415de32b317bec7d6c3cc6eede74d596380e74
Satita Yhupook <satita.y@ku.th>
2025-09-18 15:15:33 +0700

    st

--- assignments/week09/random_quiz-2.py
+++ assignments/week09/random_quiz-2.py
@@ -58,8 +58,47 @@ def get_divisibility_hint(number):
 
 def get_range_hint(number, current_min=1, current_max=100):
     # Return narrowed range around the number
-    pass
+    return f"HINT: The narrowed range {range(current_min, current_max)}"
 
 def get_thefirst_digit_hint(number):
     # Retun the first digit of the number
-    pass
+    
+
+    import random
+
+# สุ่มเลขระหว่าง 0 - 9
+test_random = random.randint(1, 20)
+
+print("-- เกมทายตัวเลข มาเดาใจคอมพิวเตอร์  --")
+print("-- ทายเลขจำนวนเต็มตั้งแต่ 1 - 100 --")
+print("-- คุณมีโอกาส 6 ครั้ง --")
+
+i = 0
+
+while True:
+
+    # รับค่าทายเลขจากผู้ใช้
+    print(f"ความพยายามครั้งที่ {i+1}")
+    guess_number = int(input("What is your guess number (1-20)?: "))
+
+    # condition ==> if-elif-else
+    if test_random == guess_number:
+        print("เก่งมาก มั่วถูกตั้งแต่ครั้งแรก เทพจริงๆ")
+        break
+
+    elif guess_number < test_random:
+        print("ผิดจร้า น้อยไปเนอะ")
+
+    else:
+        print("ผิดจร้า มากไปหน่อย")
+
+    if i == 3:
+       print(get_parity_hint(test_random))
+    elif i == 5:
+       print(get_range_hint(test_random))
+    elif i == 7:
+       print(get_range_hint(test_random, test_random-12, test_random+12))
+    elif i == 10:
+       print(get_thefirst_digit_hint(test_random))
+             
+    i = i + 1
--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,38 @@
 import random
 
-def test_random():
-    random_number = random.randint(1, 100)
-    print(random_number)
-    
-test_random()
+# สุ่มเลขระหว่าง 0 - 9
+test_random = random.randint(1, 20)
+
+print("-- เกมทายตัวเลข มาเดาใจคอมพิวเตอร์  --")
+print("-- ทายเลขจำนวนเต็มตั้งแต่ 1 - 100 --")
+print("-- คุณมีโอกาส 6 ครั้ง --")
+
+i = 0
+
+while True:
+
+    # รับค่าทายเลขจากผู้ใช้
+    print(f"ความพยายามครั้งที่ {i+1}")
+    guess_number = int(input("What is your guess number (1-20)?: "))
+
+    # condition ==> if-elif-else
+    if test_random == guess_number:
+        print("เก่งมาก มั่วถูกตั้งแต่ครั้งแรก เทพจริงๆ")
+        break
+
+    elif guess_number < test_random:
+        print("ผิดจร้า น้อยไปเนอะ")
+
+    else:
+        print("ผิดจร้า มากไปหน่อย")
+
+    if i == 3:
+        print(get_parity_hint(test_random))
+    elif i == 5:
+        print(get_range_hint(test_random))
+    elif i == 7:
+        print(get_range_hint(test_random, test_random-12, test_random+12))
+    elif i == 10:
+        print(get_thefirst_digit_hint(test_random))
+             
+    i = i + 1
